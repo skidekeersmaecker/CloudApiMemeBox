dd575d7ed4bce0bb927b74953ded3c498207a52a
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2017-06-08 20:19:03 +0200

    app werkt

--- UI/client.js
+++ UI/client.js
@@ -6,33 +6,41 @@ app.controller("mainControlsController", function($scope, $http, $interval) {
   mainControls.images = [];
 
   // TODO Build functionality to fetch images
-
-  $http.get("http://localhost:3000/api/getDb").success(function (res) {
-      $scope.db = res;
-
-      console.log("      *****db*****\n");
-      for(i = 0; i < $scope.db.images.length; i++){
-          console.log("url: " + $scope.db.images[i].url + "\noriginal_text: " + $scope.db.images[i].original_text + "\n");
-      }
-      console.log("\n      ************");
-  });
+  getDB();
 
   // Form behavior
   mainControls.inputText = "";
+
+  //op save button click
   mainControls.troll = function() {
     // TODO Send the input text to the server and let the server create an appropriate meme.
 
+    //maak json van bestand om door te sturen
     $scope.text = { 'text': $scope.mainControls.inputText};
     console.log("entered: " + $scope.text.text);
+
+    //post input naar server side (start.js)
     $http.post("http://localhost:3000/api/inputText", $scope.text).success(function (res) {
-      //refreshData();
       console.log("send: " + $scope.text);
     });
 
 
     // TODO Then refresh the images, which should now include the new one
-      //zie in post functie hier boven
+    getDB();
+
   };
 
   // TODO Refresh every 3 seconds
+  $interval(getDB, 3000);
+
+  function getDB() {
+    $http.get("http://localhost:3000/api/getDb").success(function (res) {
+      mainControls.images = res.images;
+      console.log("      *****db*****\n");
+      for (i = 0; i < mainControls.images.length; i++) {
+        console.log("url: " + mainControls.images[i].url + "\noriginal_text: " + mainControls.images[i].original_text + "\n");
+      }
+      console.log("\n      ************");
+    });
+  }
 });
